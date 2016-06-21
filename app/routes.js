61f75b3da5b7b47cc602ef56a7549c620564e9d9
demiurgosoft <demiurgosoft@hotmail.com>
2016-06-21 10:54:28 +0200

    un poco de maraña limpiada #10

--- app/routes.js
+++ app/routes.js
@@ -1,22 +1,27 @@
 var jwt = require('jsonwebtoken');
 var useragent = require('useragent');
-var express=require('express');
+var express = require('express');
 
-var config=require('./config');
+var config = require('./config');
 
-var database=require('./database');
-var db=database.db;
-var polls=database.polls;
+var database = require('./database');
+var db = database.db;
+var polls = database.polls;
 
-module.exports=function(app){
-    
-// Rutas
-app.get('/:id', function(req, res) {
-    var ses = req.session;
 
-    if (typeof polls[req.params.id] === 'undefined') {
-        res.status(404).send('ID ' + req.params.id + ' not found');
-    } else {
+module.exports = function(app) {
+
+    app.use('/:id', function(req, res, next) {
+        if (typeof polls[req.params.id] === 'undefined')
+            res.status(404).json({
+                error: 'ID ' + req.params.id + ' not found'
+            });
+        else next();
+    });
+    // Rutas
+    app.get('/:id', function(req, res) {
+        var ses = req.session;
+
         var agent = useragent.parse(req.headers['user-agent']);
         var dev_id = (agent.toAgent() +
             "_" + agent.os.toString() + "_" +
@@ -34,98 +39,87 @@ app.get('/:id', function(req, res) {
         });
         db.saveDatabase();
 
-	var payload = {
-	    success: true,
-	    message: 'Token creado',
-	    token: token,
-	    poll: polls[req.params.id]
+        var payload = {
+            success: true,
+            message: 'Token creado',
+            token: token,
+            poll: polls[req.params.id]
         };
 
-	console.log( req.params );
-        res.json( payload );
-	    
-    }
-});
+        console.log(req.params);
+        res.json(payload);
+
+    });
 
 
-app.get('/:id/p/:f', function(req, res) {
-    var ses = req.session;
+    app.get('/:id/p/:f', function(req, res) {
+        var ses = req.session;
+            var agent = useragent.parse(req.headers['user-agent']);
+            var dev_id = (agent.toAgent() +
+                "_" + agent.os.toString() + "_" +
+                agent.device.toString()).replace(/\s/g, "");
+            var value = "ID:" + req.params.id + "_" + dev_id;
 
-    if (typeof polls[req.params.id] === 'undefined') {
-        res.status(404).send('ID ' + req.params.id + ' not found');
-    } else {
-        var agent = useragent.parse(req.headers['user-agent']);
-        var dev_id = (agent.toAgent() +
-            "_" + agent.os.toString() + "_" +
-            agent.device.toString()).replace(/\s/g, "");
-        var value = "ID:" + req.params.id + "_" + dev_id;
+            // Crea el token
+            var token = jwt.sign(value, config.secret);
+            ses.token = token;
 
-        // Crea el token
-        var token = jwt.sign(value, config.secret);
-        ses.token = token;
+            var poll_collection = polls[req.params.id];
+            poll_collection.insert({
+                dev_id: dev_id,
+                token: token
+            });
+            db.saveDatabase();
 
-        var poll_collection = polls[req.params.id];
-        poll_collection.insert({
-            dev_id: dev_id,
-            token: token
-        });
-        db.saveDatabase();
+            console.log(req.params);
+            res.header("Content-Type", "application/javascript");
+            res.send(req.params.f + "( " + JSON.stringify(polls[req.params.id]) + ")");
+    });
 
-	console.log( req.params );
-	res.header("Content-Type", "application/javascript");
-	res.send(req.params.f + "( " + JSON.stringify(  polls[req.params.id] ) + ")");
-	    
-    }
-});
+    app.put('/:id/:token/:respuesta', function(req, res) {
+            var poll_collection = polls[req.params.id];
+            console.log(req.params.token);
+            var dev_id = poll_collection.find({
+                'token': req.params.token
+            });
+            res.json({
+                success: true,
+                message: 'token correcto'
+            });
+    });
 
-app.put('/:id/:token/:respuesta', function(req, res) {
-    if (typeof polls[req.params.id] === 'undefined') {
-        res.status(404).send('ID ' + req.params.id + ' not found');
-    } else {
-        var poll_collection = polls[req.params.id];
-        console.log(req.params.token);
-        var dev_id = poll_collection.find({
-            'token': req.params.token
-        });
-        res.json({
-            success: true,
-            message: 'token correcto'
-        });
-    }
-});
+    // Rutas protegidas
+    var api = express.Router();
+    app.use('/:id', api);
 
-// Rutas protegidas
-var api = express.Router();
-app.use('/:id', api);
+    // Middleware para verficiar el token
+    api.use(function(req, res, next) {
+        // Obtener el token
+        var ses = req.session;
 
-// Middleware para verficiar el token
-api.use(function(req, res, next) {
-    // Obtener el token
-    var ses = req.session;
+        // Verificar el token
+        if (ses.token) {
+            jwt.verify(ses.token, config.secret, function(err, value) {
+                if (err) {
+                    return res.json(403, {
+                        success: false,
+                        message: 'Fallo al verificar el token. Acceso no autorizado'
+                    });
+                } else {
+                    // Se pasa el token con la solicitud
+                    req.value = value;
+                    next();
+                }
+            });
+        } else {
+            return res.status(404).send({
+                success: false,
+                message: 'Token no encontrado.'
+            });
+        }
+    });
 
-    // Verificar el token
-    if (ses.token) {
-        jwt.verify(ses.token, config.secret, function(err, value) {
-            if (err) {
-                return res.json(403, {
-                    success: false,
-                    message: 'Fallo al verificar el token. Acceso no autorizado'
-                });
-            } else {
-                // Se pasa el token con la solicitud
-                req.value = value;
-                next();
-            }
-        });
-    } else {
-        return res.status(404).send({
-            success: false,
-            message: 'Token no encontrado.'
-        });
-    }
-});
-
-api.get('/check', function(req, res) {
-    res.json(req.value);
-});
+    api.get('/check', function(req, res) {
+        res.json(req.value);
+    });
 };
