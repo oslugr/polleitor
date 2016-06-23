52b21f1cb42c4d980edaf040d1f6dd5d91910462
German Martinez <germaaan@gmail.com>
2016-06-23 12:44:33 +0200

    Añade middleware para verificación/creación del token inicialmente

--- app/routes.js
+++ app/routes.js
@@ -6,10 +6,42 @@ var config = require('./config');
 
 
 
-module.exports = function(app,handler) {
+module.exports = function(app, handler) {
+
+    // Middleware para crear/verificar token el token
+    app.use(function(req, res, next) {
+        // Obtener el token
+        var ses = req.session;
+
+        // Verificar el token, creándolo si no existe
+        if (ses.token) {
+            jwt.verify(ses.token, config.secret, function(err, value) {
+                if (err) {
+                    return res.json(403, {
+                        success: false,
+                        message: 'Fallo al verificar el token. Acceso no autorizado'
+                    });
+                } else {
+                    next();
+                }
+            });
+        } else {
+            var agent = useragent.parse(req.headers['user-agent']);
+            var dev_id = (agent.toAgent() +
+                "_" + agent.os.toString() + "_" +
+                agent.device.toString()).replace(/\s/g, "");
+            var value = "ID:" + req.params.id + "_" + dev_id;
+
+            // Crea el token
+            var token = jwt.sign(value, config.secret);
+            ses.token = token;
+
+            next();
+        }
+    });
 
     app.use('/:poll', function(req, res, next) {
-        var poll=req.params.poll;
+        var poll = req.params.poll;
         if (!handler.checkPoll(poll))
             res.status(404).json({
                 error: 'Poll ' + poll + ' not found'
@@ -22,38 +54,22 @@ module.exports = function(app,handler) {
         //Esto no deberia estar aqui
         var ses = req.session;
 
-        var agent = useragent.parse(req.headers['user-agent']);
-        var dev_id = (agent.toAgent() +
-            "_" + agent.os.toString() + "_" +
-            agent.device.toString()).replace(/\s/g, "");
-        var value = "ID:" + req.params.id + "_" + dev_id;
-
-        // Crea el token
-        var token = jwt.sign(value, config.secret);
-        ses.token = token;
-
-    /*    var db_collection = req.db_collection;
-        db_collection.insert({
-            client: {
-                dev_id: dev_id,
-                token: token
-            }
+        var questions = handler.getPoll(req.params.poll);
+        if (!questions) return res.status(404).json({
+            status: 404,
+            error: "Not Poll Found",
+            poll: req.params.poll
         });
-        db.saveDatabase();*/
-        
-        var questions=handler.getPoll(req.params.poll);
-        if(!questions) return res.status(404).json({status:404,error:"Not Poll Found",poll:req.params.poll});
-        else{
+        else {
             res.json(questions);
-            
         }
-    
-
     });
-    
+
     app.get('/:poll/resultados', function(req, res) {
-        var answers=handler.getAnswersPoll(req.params.poll);
-        if(!answers) return res.status(404).json({error:'poll '+req.params.poll+' not found'});
+        var answers = handler.getAnswersPoll(req.params.poll);
+        if (!answers) return res.status(404).json({
+            error: 'poll ' + req.params.poll + ' not found'
+        });
         else return res.json(answers);
     });
 
