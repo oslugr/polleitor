5016f2f15f49f56c29884e377a4e3b134bebdece
German Martinez <germaaan@gmail.com>
2016-06-24 11:32:28 +0200

    Elimina cosas innecesarias del token

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,7 @@ var express = require('express');
 var config = require('./config');
 
 
-
 module.exports = function(app, handler) {
-
     // Middleware para crear/verificar token el token
     app.use(function(req, res, next) {
         // Obtener el token
@@ -19,7 +17,7 @@ module.exports = function(app, handler) {
                 if (err) {
                     return res.json(403, {
                         success: false,
-                        message: 'Fallo al verificar el token. Acceso no autorizado'
+                        message: 'Failed to verify the token. Unauthorized access.'
                     });
                 } else {
                     next();
@@ -28,9 +26,9 @@ module.exports = function(app, handler) {
         } else {
             var agent = useragent.parse(req.headers['user-agent']);
             var dev_id = (agent.toAgent() +
-                "_" + agent.os.toString() + "_" +
-                agent.device.toString()).replace(/\s/g, "");
-            var value = "ID:" + req.params.id + "_" + dev_id;
+                '_' + agent.os.toString() + '_' +
+                agent.device.toString()).replace(/\s/g, '');
+            var value = 'ID:' + req.params.id + '_' + dev_id;
 
             // Crea el token
             var token = jwt.sign(value, config.secret);
@@ -42,25 +40,27 @@ module.exports = function(app, handler) {
 
     app.use('/:poll', function(req, res, next) {
         var poll = req.params.poll;
-        if (!handler.checkPoll(poll))
+
+        if (!handler.checkPoll(poll)) {
             res.status(404).json({
-                error: 'Poll ' + poll + ' not found'
+                error: 'Poll ' + poll + ' not found.'
             });
-        else next();
+        } else {
+            next();
+        }
     });
 
     // Muestra preguntas y posibles respuestas del poll
     app.get('/:poll', function(req, res) {
-        //Esto no deberia estar aqui
-        var ses = req.session;
-
         var questions = handler.getPoll(req.params.poll);
-        if (!questions) return res.status(404).json({
-            status: 404,
-            error: "Not Poll Found",
-            poll: req.params.poll
-        });
-        else {
+
+        if (!questions) {
+            return res.status(404).json({
+                status: 404,
+                error: 'Not poll found.',
+                poll: req.params.poll
+            });
+        } else {
             res.json(questions);
         }
     });
@@ -68,24 +68,25 @@ module.exports = function(app, handler) {
     // Muestra resultados del poll
     app.get('/:poll/resultados', function(req, res) {
         var answers = handler.getAnswersPoll(req.params.poll);
-        if (!answers) return res.status(404).json({
-            error: 'poll ' + req.params.poll + ' not found'
-        });
-        else return res.json(answers);
+
+        if (!answers) {
+            return res.status(404).json({
+                error: 'Poll ' + req.params.poll + ' not found.'
+            });
+        } else {
+            return res.json(answers);
+        }
     });
 
 
     app.get('/:poll/p/:f', function(req, res) {
-        var ses = req.session;
+        var token = req.session.token;
+
         var agent = useragent.parse(req.headers['user-agent']);
         var dev_id = (agent.toAgent() +
-            "_" + agent.os.toString() + "_" +
-            agent.device.toString()).replace(/\s/g, "");
-        var value = "ID:" + req.params.id + "_" + dev_id;
-
-        // Crea el token
-        var token = jwt.sign(value, config.secret);
-        ses.token = token;
+            '_' + agent.os.toString() + '_' +
+            agent.device.toString()).replace(/\s/g, '');
+        var value = 'ID:' + req.params.id + '_' + dev_id;
 
         var db_collection = req.db_collection; // Código repetido
         db_collection.insert({
@@ -99,59 +100,30 @@ module.exports = function(app, handler) {
         console.log(req.params);
         var poll_to_send = {};
         poll_to_send[req.params.id] = req.poll;
-        res.header("Content-Type", "application/javascript");
-        res.send(req.params.f + "( " + JSON.stringify(poll_to_send) + ")");
+        res.header('Content-Type', 'application/javascript');
+        res.send(req.params.f + '( ' + JSON.stringify(poll_to_send) + ')');
     });
 
     app.put('/:poll/:pregunta/:respuesta', function(req, res) {
         var token = req.session.token;
+
         var r = handler.answerQuestion(req.params.poll, req.params.pregunta, req.params.respuesta, token);
-        if (r) return res.json({
-            status: "OK",
-            ok: true,
-            poll: req.params.poll,
-            pregunta: req.params.pregunta
-        });
-        else return res.json({
-            status: "FAIL",
-            ok: false,
-            poll: req.params.poll,
-            pregunta: req.params.pregunta
-        });
-    });
 
-    // Rutas protegidas
-    var api = express.Router();
-    app.use('/:poll', api);
-
-    // Middleware para verficiar el token
-    api.use(function(req, res, next) {
-        // Obtener el token
-        var ses = req.session;
-
-        // Verificar el token
-        if (ses.token) {
-            jwt.verify(ses.token, config.secret, function(err, value) {
-                if (err) {
-                    return res.json(403, {
-                        success: false,
-                        message: 'Fallo al verificar el token. Acceso no autorizado'
-                    });
-                } else {
-                    // Se pasa el token con la solicitud
-                    req.value = value;
-                    next();
-                }
+        if (r) {
+            return res.json({
+                status: 'OK',
+                ok: true,
+                poll: req.params.poll,
+                pregunta: req.params.pregunta
             });
         } else {
-            return res.status(404).send({
-                success: false,
-                message: 'Token no encontrado.'
+            return res.json({
+                status: 'FAIL',
+                ok: false,
+                poll: req.params.poll,
+                pregunta: req.params.pregunta
             });
         }
     });
 
-    api.get('/check', function(req, res) {
-        res.json(req.value);
-    });
 };
