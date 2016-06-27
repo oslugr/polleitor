4860fb0a4d7aaa2c83dd45f88fad92d2b66f339e
German Martinez <germaaan@gmail.com>
2016-06-27 13:35:33 +0200

    Muestra preguntas. Ref #14

--- public/scripts/polleitor.js
+++ public/scripts/polleitor.js
@@ -1,26 +1,37 @@
-function getPoll(poll,done){
+function getPoll(poll, done) {
     $.ajax({
-			url: '/'+poll,
-			type: "GET",
-			dataType: 'json',
-			cache: false,
-			success: function(data) {
-				done(null, data);
-			},
-			error: function(xhr, status, err) {
-				done(new Error(err));
-			}
-		});    
+        url: '/' + poll,
+        type: "GET",
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+            done(null, data);
+        },
+        error: function(xhr, status, err) {
+            done(new Error(err));
+        }
+    });
 }
 
-
-$(function(){
-    var name=$(".poll").attr('data-poll-name');
+$(function() {
+    var name = $(".poll").attr('data-poll-name');
     console.log(name);
-    getPoll(name,function(err,res){
-        if(err) console.log(err);
-        else {
-            $(".poll").text(JSON.stringify(res));
+    getPoll(name, function(err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            $.each(res, function(index, value) {
+                console.log(value);
+                $(".poll").append("<p>" + value.question + "</p>");
+                $(".poll").append("<ul>");
+                $.each(value.options, function(index, value) {
+                    //console.log(value);
+                    $(".poll").append("<li>" + value + "</li>");
+                });
+                $(".poll").append("</ul>");
+            });
+
+            //$(".poll").text(JSON.stringify(res));
         }
     });
 });
