4a4d6aa37f4cdd8c467ea789bbae16155409a8b9
German Martinez <germaaan@gmail.com>
2016-06-28 12:23:26 +0200

    Recoge respuestas. Ref #14

--- public/scripts/polleitor.js
+++ public/scripts/polleitor.js
@@ -1,49 +1,51 @@
 function getPoll(poll, done) {
     $.ajax({
-			url: '/'+poll,
-			type: "GET",
-			cache: false,
-			success: function(data) {
-				done(null, data);
-			},
-			error: function(xhr, status, err) {
-				done(new Error(err));
-			}
-		});    
+        url: '/' + poll,
+        type: "GET",
+        cache: false,
+        success: function(data) {
+            done(null, data);
+        },
+        error: function(xhr, status, err) {
+            done(new Error(err));
+        }
+    });
 }
 
-function getAnswers(poll,done){
+function getAnswers(poll, done) {
     $.ajax({
-            url: '/'+poll+'/resultados',
-            type: "GET",
-            cache: false,
-            success: function(data) {
-                done(null, data);
-            },
-            error: function(xhr, status, err) {
-                done(new Error(err));
-            }
-        });      
-}
-function postAnswers(poll,answers,done){
-    $.ajax({
-            url: '/'+poll,
-            type: "POST",
-            data: answers,
-            dataType: 'json',
-            cache: false,
-            success: function(data) {
-                done(null, data);
-            },
-            error: function(xhr, status, err) {
-                done(new Error(err));
-            }
-        });      
+        url: '/' + poll + '/resultados',
+        type: "GET",
+        cache: false,
+        success: function(data) {
+            done(null, data);
+        },
+        error: function(xhr, status, err) {
+            done(new Error(err));
+        }
+    });
 }
 
-$(function(){
-    var name=$(".poll").attr('data-poll-name');
+function postAnswers(poll, answers, done) {
+    $.ajax({
+        url: '/' + poll,
+        type: "POST",
+        data: answers,
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+            done(null, data);
+        },
+        error: function(xhr, status, err) {
+            done(new Error(err));
+        }
+    });
+}
+
+$(function() {
+    var name = $(".poll").attr('data-poll-name');
     console.log(name);
+
     getPoll(name, function(err, res) {
         if (err) {
             console.log(err);
@@ -51,13 +53,31 @@ $(function(){
             $.each(res, function(id_p, val_p) {
                 console.log(val_p);
                 $(".poll").append("<p>" + val_p.question + "</p>");
+                var def = false;
+
                 $.each(val_p.options, function(id_r, val_r) {
-                    //console.log(value);
-                    $(".poll").append("<input type='radio' name='" + id_p + "' value='" + id_r + "'>" + val_r + "<br>");
+                    if (!def) {
+                        $(".poll").append("<input type='radio' name='" + id_p + "' value='" + id_r + "' checked='checked'>" + val_r + "<br>");
+                        def = true;
+                    } else {
+                        $(".poll").append("<input type='radio' name='" + id_p + "' value='" + id_r + "'>" + val_r + "<br>");
+                    }
                 });
             });
 
             //$(".poll").text(JSON.stringify(res));
         }
     });
+
+    $("#enviar").click(function() {
+        var answers = new Array();
+
+        $.each($('input[type=radio]'), function() {
+            if ($(this).is(":checked")) {
+                console.log("Question: " + $(this).attr("name") + " - Answer: " + $(this).val());
+                answers.push($(this).val());
+            }
+        });
+    });
+
 });
