ff983979b2d4c1e76638744dd6fe133a7beed550
JJ Merelo <jjmerelo@gmail.com>
2016-06-28 19:48:15 +0200

    Probando con dos polls
    A ver si había algún problema. Closes #18

--- app/config.js
+++ app/config.js
@@ -5,6 +5,10 @@ module.exports = {
         "asignatura": [{
             "q": "¿Están bien preparados los alumnos de tu(s) asignatura(s)?",
             "a": ["Si", "No"]
-        }]
+        }],
+	"TFG": [{
+	    "q": "¿Alguna vez has pensado que un alumno es incapaz de hacer un TFG de cierta entidad?",
+	    "a": ["Sí", "No"]
+	}]
     }
 };
