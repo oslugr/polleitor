d5a37ae7900ba6dbaf654e688d9347cd4d9d9cb2
JJ Merelo <jjmerelo@gmail.com>
2016-06-30 08:17:46 +0200

    Integrado con reveal.js
    Había otro error en el código de polleitor que, aunque diferenciaba entre polls, usaba $(.poll) con lo que se añadía todo a no sé muy bien qué. Ya se usa $(this) de forma razonable.
    
    Closes #15

--- public/scripts/polleitor.js
+++ public/scripts/polleitor.js
@@ -46,58 +46,61 @@ function postAnswers(poll, answers, done) {
 
 $(function() {
     $(".poll").each( function() {
-	var name =  $(this).attr( 'data-poll-name');
+	var this_div = $(this);
+	var name =  this_div.attr( 'data-poll-name');
 	console.log(name);
+
 	getPoll(name, function(err, res) {
-	console.log( "getPoll " + name );
-        if (err) {
-            console.log(err);
-        } else {
-            $.each(res, function(id_que, val_que) {
-                console.log(val_que);
-                $(".poll").append("<p>" + val_que.question + "</p>");
-                var def = false;
-
-                $.each(val_que.options, function(id_ans, val_ans) {
-                    if (!def) {
-                        $(".poll").append("<input type='radio' name='" + val_que.id + "' value='" + id_ans + "' checked='checked'>" + val_ans + "<br>");
-                        def = true;
-                    } else {
-                        $(".poll").append("<input type='radio' name='" + val_que.id + "' value='" + id_ans + "'>" + val_ans + "<br>");
-                    }
-                });
-
-                $(".poll").append("<br><button type='button' class='submit' id='" + val_que.id + "'>Enviar respuestas</button>");
-
-                $(".submit").click(function() {
-                    function Answer(id, answer) {
-                        this.id = id;
-                        this.answer = answer;
-                    }
-
-                    var answers = [];
-
-                    $.each($('input[type=radio]'), function() {
-                        if ($(this).is(":checked")) {
-                            //console.log("Question: " + $(this).attr("name") + " - Answer: " + $(this).val());
-                            var answer = new Answer($(this).attr("name"), $(this).val());
-                            answers.push(answer);
-                        }
+	    console.log( "getPoll " + name );
+	    
+            if (err) {
+		console.log(err);
+            } else {
+		$.each(res, function(id_que, val_que) {
+                    console.log(val_que);
+                    this_div.append("<p>" + val_que.question + "</p>");
+                    var def = false;
+		    
+                    $.each(val_que.options, function(id_ans, val_ans) {
+			if (!def) {
+                            this_div.append("<input type='radio' name='" + val_que.id + "' value='" + id_ans + "' checked='checked'>" + val_ans + "<br>");
+                            def = true;
+			} else {
+                            this_div.append("<input type='radio' name='" + val_que.id + "' value='" + id_ans + "'>" + val_ans + "<br>");
+			}
                     });
-
-                    //console.log(JSON.stringify(answers));
-
-                    postAnswers(name, answers, function(err, res) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log(res);
-                        }
+		    
+                    this_div.append("<br><button type='button' class='submit' id='" + val_que.id + "'>Enviar respuestas</button>");
+		    
+                    $(".submit").click(function() {
+			function Answer(id, answer) {
+                            this.id = id;
+                            this.answer = answer;
+			}
+			
+			var answers = [];
+			
+			$.each($('input[type=radio]'), function() {
+                            if (this_div.is(":checked")) {
+				//console.log("Question: " + this_div.attr("name") + " - Answer: " + this_div.val());
+				var answer = new Answer(this_div.attr("name"), this_div.val());
+				answers.push(answer);
+                            }
+			});
+			
+			//console.log(JSON.stringify(answers));
+			
+			postAnswers(name, answers, function(err, res) {
+                            if (err) {
+				console.log(err);
+                            } else {
+				console.log(res);
+                            }
+			});
                     });
-                });
-
-            });
-        }
+		    
+		});
+            }
 	});
     });
     
