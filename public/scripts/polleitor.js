5a782bb103d2195b1b1575665bb4b8c3267458e1
JJ Merelo <jjmerelo@gmail.com>
2016-06-30 11:48:14 +0200

    Cambiadas las encuestas a botones
    Refs #24 y #14 pero todavía falta enviar la info correctamente

--- public/scripts/polleitor.js
+++ public/scripts/polleitor.js
@@ -50,59 +50,47 @@ $(function() {
 	var this_div = $(this);
 	var name =  this_div.attr( 'data-poll-name');
 	console.log(name);
-
+	
 	getPoll(name, function(err, res) {
 	    console.log( "getPoll " + name );
-	    
+	    console.log( res );
             if (err) {
 		console.log(err);
             } else {
-		$.each(res, function(id_que, val_que) {
-                    console.log(val_que);
-                    this_div.append("<p>" + val_que.question + "</p>");
-                    var def = false;
+		for ( var i in res ) {
+                    console.log(res[i]);
+                    this_div.append("<p>" + res[i].question + "</p>");
+		    //                    var def = false;
 		    
-                    $.each(val_que.options, function(id_ans, val_ans) {
-			if (!def) {
-                            this_div.append("<input type='radio' name='" + name + "-" + val_que.id + "' value='" + id_ans + "' checked='checked'>" + val_ans + "<br>");
-                            def = true;
-			} else {
-                            this_div.append("<input type='radio' name='" + val_que.id + "' value='" + id_ans + "'>" + val_ans + "<br>");
-			}
-                    });
+                    for ( var j in res[i].options) {
+			this_div.append("<button type='button' class='submit' data-question='"
+					+ i + "' data-poll='"+name+"' id='" + j + "' value='" + res[i].id
+					+  "'name='" +name + "-" + j + "'>" + res[i].options[j] + "</button>");
+                    }
 		    
-                    this_div.append("<br><button type='button' class='submit' id='" + val_que.id + "'>Enviar respuestas</button>");
 		    
-                    $(".submit").click(function() {
-			function Answer(id, answer) {
-                            this.id = id;
-                            this.answer = answer;
-			}
-			
-			var answers = [];
-			
-			$.each($('input[type=radio]'), function() {
-                            if ($(this).is(":checked")) {
-				console.log("Question: " + this_div.attr("name") + " - Answer: " + this_div.val());
-				var answer = new Answer(this_div.attr("name"), this_div.val());
-				answers.push(answer);
-                            }
-			});
-			
-			console.log(JSON.stringify(answers));
-			
-			postAnswers(name, answers, function(err, res) {
-                            if (err) {
-				console.log(err);
-                            } else {
-				console.log(res);
-                            }
-			});
-                    });
-		    
-		});
+		};
             }
 	});
+
+	$(".submit").click(function() {
+	    function Answer(id, answer) {
+		this.id = id;
+		this.answer = answer;
+	    }
+	    
+	    var answers = [];
+	    
+	    console.log(JSON.stringify(answers));
+	    
+	    postAnswers(name, answers, function(err, res) {
+		if (err) {
+		    console.log(err);
+		} else {
+		    console.log(res);
+		}
+	    });
+	});
+	
     });
-    
 });
