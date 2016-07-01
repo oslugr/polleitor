16b6f1e969ffa70a26824b3c229ecde422570897
JJ Merelo <jjmerelo@gmail.com>
2016-07-01 18:38:38 +0200

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -39,6 +39,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "type": "heading",
               "data": {
                 "level": 2,
+                "title": "Documentación",
+                "slug": "documentacin"
+              },
+              "depth": 2
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 2,
                 "title": "API",
                 "slug": "api"
               },
@@ -56,6 +64,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "level": 2,
             "title": "Instalación",
             "slug": "instalacin"
+          },
+          "depth": 2
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
+            "title": "Documentación",
+            "slug": "documentacin"
           },
           "depth": 2
         }, {
